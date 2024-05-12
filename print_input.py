aa49d15a7088acc126c3ad9b7955e13ba53d349a
E-ternalSpring <espindolaemmanuelle@gmail.com>
2024-05-11 21:59:25 -0300

    adicionar exercícios iniciais: hello_world, print_input, variaveis_constantes, data_types e convertendo_tipos

---
+++ print_input.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome chuchu? ")
+idade = input("E a sua idade? ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Emmanuelle"
+idade = 27
+
+nome, idade = "André", 25
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+ESTADOS_BRASILEIROS = ["SP", "RJ", "SC", "RS"]
+
+print(ESTADOS_BRASILEIROS)
